91fc8432a0587827149120dea76a9d6958a5730f
acxz <akashpatel2008@yahoo.com>
2017-04-23 01:04:45 -0400

    Add files via upload

--- prob.py
+++ prob.py
@@ -1,3 +1,6 @@
+# MATH 3670 Article Clustering
+# Authors: Akash Patel, Nabeel Mahmood, Taylor Fields
+
 import statistics as stat
 from nltk.corpus import reuters
 import os
@@ -12,7 +15,7 @@ os.listdir('/home/apatel435/nltk_data/corpora/reuters/training/')
 
 wordlist = [];
 num_articles = 0
-artnum = 10
+artnum = 1000
 j = 0
 for filename in os.listdir('/home/apatel435/nltk_data/corpora/reuters/training/'):
     if j < artnum:
@@ -30,22 +33,58 @@ wordlist = list(set(wordlist))
 # Sort
 wordlist.sort()
 
-#print(wordlist)
-#print('     ')
-
-### TODO Filter common words and special characters
-### can based it on words that only occur once
+### Filter common words and special characters
 # take out numbers
 wordlist = [ x for x in wordlist if x.isalpha() ]
 
-### ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# Filter common words
+common = ["a", "about", "above", "above", "across", "after", "afterwards",
+"again", "against", "all", "almost", "alone", "along", "already",
+"also","although","always","am","among", "amongst", "amongst", "amount",  "an",
+"and", "another", "any","anyhow","anyone","anything","anyway", "anywhere",
+"are", "around", "as",  "at", "back","be","became",
+"because","become","becomes", "becoming", "been", "before", "beforehand",
+"behind", "being", "below", "beside", "besides", "between", "beyond", "bill",
+"both", "bottom","but", "by", "call", "can", "cannot", "cant", "co", "con",
+"could", "couldnt", "do", "done", "down",
+"due", "during", "each", "e.g.", "eight", "either", "eleven","else",
+"elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone",
+"everything", "everywhere", "except", "few", "fifteen", "fifty", "fill", "find",
+"fire", "first", "five", "for", "former", "formerly", "forty", "found", "four",
+"from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt",
+"have", "he", "hence", "her", "here", "hereafter", "hereby", "herein",
+"hereupon", "hers", "herself", "him", "himself", "his", "how", "however",
+"hundred", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it",
+"its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd",
+"made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more",
+"moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name",
+"namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody",
+"none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off",
+"often", "on", "once", "one", "only", "onto", "or", "other", "others",
+"otherwise", "our", "ours", "ourselves", "out", "over", "own","part", "per",
+"perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed",
+"seeming", "seems", "serious", "several", "she", "should", "show", "side",
+"since", "sincere", "six", "sixty", "so", "some", "somehow", "someone",
+"something", "sometime", "sometimes", "somewhere", "still", "such",
+"take", "ten", "than", "that", "the", "their", "them", "themselves", "then",
+"thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon",
+"these", "they", "thick", "thin", "third", "this", "those", "though", "three",
+"through", "throughout", "thru", "thus", "to", "together", "too", "top",
+"toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up",
+"upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever",
+"when", "whence", "whenever", "where", "whereafter", "whereas", "whereby",
+"wherein", "whereupon", "wherever", "whether", "which", "while", "whither",
+"who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within",
+"without", "would", "yet", "you", "your", "yours", "yourself", "yourselves",
+"the"]
 
-#print(wordlist)
+wordlist = [x for x in wordlist if x not in common]
+
 
 num_wordlist = len(wordlist)
 
 ### Continue with matrix building
-
+# Add a 1 if the article contains the word in wordlist
 arr = np.zeros((num_wordlist,num_articles))
 
 i = 0
@@ -73,12 +112,9 @@ for x in range(1,artnum):
 
 # Covariance matrix
 cov_arr = np.cov(stackarr)
-#print("Covariance")
-print(cov_arr)
-print(" ")
 
 ### Perform k - means algo clustering
-
+# Cluster size chosen to be 5 due to relatively large dataset (1000x1000)
 clusters = 5
 
 centroids,_= kmeans(cov_arr,clusters)
@@ -94,5 +130,5 @@ cov_arrsort = cov_arr[index];
 # Transpose matrix to sort the rows
 # Matrix is symmetric so transposing it produces the same result
 cov_arrtrans = cov_arrsort.transpose();
-cov_arrsort = cov_arrtrans[index];
-print(cov_arrsort)
+cluster_cov_arr = cov_arrtrans[index];
+print(cluster_cov_arr)
